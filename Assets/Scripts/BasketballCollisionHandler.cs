72c733cc70190fc3c008fcf6e6d316f98272be2c
CooLFknDuDe <115779313+CooLFknDuDe@users.noreply.github.com>
2024-11-26 17:39:31 +0100

    Fuckderherskrald

---
+++ Assets/Scripts/BasketballCollisionHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketballCollionHandler : MonoBehaviour
+{
+
+    private ScoreManager scoreManager1;  // Reference to the ScoreManager
+
+    private void Start()
+    {
+        // Find the ScoreManager in the scene
+        scoreManager1 = FindObjectOfType<ScoreManager>();
+    }
+
+    // This method is called when another collider enters this cup's collider
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the object that collided with the cup has the tag "Bullet"
+        if (collision.gameObject.CompareTag("Basket"))
+        {
+
+            // Update the score in the ScoreManager
+            if (scoreManager1 != null)
+            {
+                scoreManager1.AddScore(1);
+            }
+
+
+            
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreManager1.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using TMPro;  // Import the TextMeshPro namespace
+
+public class ScoreManager1 : MonoBehaviour
+{
+    public int score = 0;  // Variable to keep track of the score
+    public TextMeshPro scoreText;  // Reference to the TextMeshPro object for displaying score
+
+    void Start()
+    {
+        // Initialize the score text in the 3D world
+        if (scoreText != null)
+        {
+            scoreText.text = score.ToString();
+        }
+    }
+
+    // Method to update the score
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScoreText();
+    }
+
+    // Method to update the displayed score text
+    private void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = score.ToString();
+        }
+    }
+}
